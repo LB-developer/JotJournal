f18ade7d3be1d005fda35ee68b243403d5dab807
lblmdesk <loganbendev@gmail.com>
2025-01-07 21:20:12 +1300

    feat(api): get user by email

--- server/service/user/store.go
+++ server/service/user/store.go
@@ -19,13 +19,60 @@ func NewStore(db *pgxpool.Pool) *Store {
 }
 
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	query := `
+	SELECT
+			*
+	FROM
+			users
+	WHERE
+			email = $1
+	`
+	rows, err := s.db.Query(context.Background(), query, email)
+	if err != nil {
+		return nil, err
+	}
+
 	user := new(types.User)
+	for rows.Next() {
+		user, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query for user failed during scan: %v\n", err)
+		return nil, err
+	}
+
+	if user.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
 	return user, nil
 }
 
 func (s *Store) GetUserByID(id int) (*types.User, error) {
 	return nil, nil
 }
+
 func (s *Store) CreateUser(user types.User) error {
 	return nil
+}
+
+func scanRowIntoUser(rows pgx.Rows) (*types.User, error) {
+	user := new(types.User)
+
+	err := rows.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
