3f4ab2363e7ab1cc300fed82f1481bb743fd9b86
lblmdesk <loganbendev@gmail.com>
2025-01-08 16:30:01 +1300

    feat(user): handle user registration
    TODO:
    - validate data

--- server/service/user/routes.go
+++ server/service/user/routes.go
@@ -1,10 +1,15 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/lb-developer/jotjournal/service/auth"
 	"github.com/lb-developer/jotjournal/types"
+	"github.com/lb-developer/jotjournal/utils"
 )
+
 type Handler struct {
 	store types.UserStore
 }
@@ -17,7 +22,41 @@ func (h *Handler) RegisterRoutes(router *chi.Mux) {
 	router.Post("/login", h.handleLogin)
 	router.Post("/register", h.handleRegisterUser)
 }
+
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
+
 func (h *Handler) handleRegisterUser(w http.ResponseWriter, r *http.Request) {
+	var user types.RegisterUserPayload
+	err := utils.ParseJSON(r, user)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// check if the user exists
+	_, err = h.store.GetUserByEmail(user.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email '%s' already exists", user.Email))
+		return
+	}
+
+	// hash users password
+	hashedPassword, err := auth.HashPassword(user.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = h.store.CreateUser(types.User{
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
