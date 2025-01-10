2132b1c35c61aaadc9f7af0169b3df9959629d26
lblmdesk <loganbendev@gmail.com>
2025-01-10 21:15:08 +1300

    fix(migrations): username -> firstName, lastName

--- server/db/migrate/migrations/000001_create_users_table.up.sql
+++ server/db/migrate/migrations/000001_create_users_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id serial PRIMARY KEY,
-    username varchar(50) UNIQUE NOT NULL,
+    firstName varchar(50) UNIQUE NOT NULL,
+    lastName varchar(50) UNIQUE NOT NULL,
     password varchar(130) NOT NULL,
     email varchar (300) UNIQUE NOT NULL
 );
