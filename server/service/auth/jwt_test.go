f52dece4406aa4d6955985f2dccd530869990a82
lblmdesk <loganbendev@gmail.com>
2025-01-11 20:29:17 +1300

    testing(JWT): token issues with correct expiration time

--- server/service/auth/jwt_test.go
+++ server/service/auth/jwt_test.go
@@ -40,3 +40,41 @@ func TestJWTSigningMethod(t *testing.T) {
 	})
 }
 
+func TestJWTExpirationTime(t *testing.T) {
+	secret := []byte("valid-secret")
+	tokenStr, _ := CreateJWT(secret, 1)
+
+	token, _ := jwt.Parse(tokenStr, nil)
+
+	if claims, ok := token.Claims.(jwt.MapClaims); !ok {
+		t.Errorf("Couldn't get JWT claims")
+	} else {
+		issuedAtClaim, exists := claims["issuedAt"]
+		if !exists {
+			t.Errorf("claims did not contain field 'issuedAt'")
+		}
+
+		expiresAtClaim, exists := claims["expiresAt"]
+		if !exists {
+			t.Errorf("claims did not contain field 'expiresAt'")
+		}
+
+		issuedAt, convert := issuedAtClaim.(float64)
+		if !convert {
+			t.Errorf("Couldn't convert issuedAt to float64")
+		}
+
+		expiresAt, converted := expiresAtClaim.(float64)
+		if !converted {
+			t.Errorf("Couldn't convert expiresAt to float64")
+		}
+
+		issued := time.Duration(issuedAt * float64(time.Second))
+		expires := time.Duration(expiresAt * float64(time.Second))
+		days := ((expires - issued) / 24).Hours()
+
+		if days != 7 {
+			t.Errorf("Issued JWT Token expiration was not 7 days")
+		}
+	}
+}
