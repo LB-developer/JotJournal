fbbbbb202703e5acc517f3476864be0a83f1784c
lblmdesk <loganbendev@gmail.com>
2025-01-11 20:24:36 +1300

    testing(JWT): empty secret returns an error

--- server/service/auth/jwt_test.go
+++ server/service/auth/jwt_test.go
@@ -19,3 +19,12 @@ func TestValidJWTCreation(t *testing.T) {
 	}
 }
 
+func TestEmptyJWTCreation(t *testing.T) {
+	secret := []byte("")
+	_, err := CreateJWT(secret, 1)
+
+	if err == nil {
+		t.Errorf("expected error for an empty secret")
+	}
+}
+
