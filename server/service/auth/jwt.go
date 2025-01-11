52d6b970bbac3a570abadf41c4aeb71a55439c7e
lblmdesk <loganbendev@gmail.com>
2025-01-11 20:22:06 +1300

    refactor(JWT): do not accept empty secret

--- server/service/auth/jwt.go
+++ server/service/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
@@ -9,9 +10,14 @@ import (
 )
 
 func CreateJWT(secret []byte, userID int) (string, error) {
+	if string(secret) == "" {
+		return "", fmt.Errorf("secret was empty when creating JWT")
+	}
+
 	expiration := time.Second * time.Duration(config.Envs.JWTExpirationInSeconds)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userID":    strconv.Itoa(userID),
+		"issuedAt":  time.Now().Unix(),
 		"expiresAt": time.Now().Add(expiration).Unix(),
 	})
 
