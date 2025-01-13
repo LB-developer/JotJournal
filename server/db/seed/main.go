f2de2b342e5af8f25488abb50d28c28a5d980fc8
lblmdesk <loganbendev@gmail.com>
2025-01-13 23:37:06 +1300

    feat(db): seed tasks
    - use faker to seed task data for user with id 1 to test api
    functionality

---
+++ server/db/seed/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/joho/godotenv"
+	"github.com/lb-developer/jotjournal/types"
+)
+
+func main() {
+	godotenv.Load()
+
+	connectionURL, found := os.LookupEnv("DATABASE_URL")
+	if !found {
+		log.Println(connectionURL)
+		log.Fatalf("Unable to find database url from .env")
+	}
+
+	db, err := sql.Open("postgres", connectionURL)
+	if err != nil {
+		log.Fatalf("Couldn't connect to database during seeding, error: %v", err)
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatalf("Couldn't get driver during seeding, error: %v", err)
+	}
+
+	_, err = migrate.NewWithDatabaseInstance(
+		"file://db/seed/seeds",
+		"postgres", driver)
+	if err != nil {
+		log.Fatalf("Seeds failed %v", err)
+	}
+
+	var task types.Task
+	createTask(&task)
+	insertUser(db, task)
+}
+
+func createTask(task *types.Task) {
+	task.Weekly = true
+	task.Monthly = false
+	task.IsCompleted = false
+
+	err := faker.FakeData(task)
+	if err != nil {
+		log.Fatalf("Couldn't create fake task, error: %v\n", err)
+	}
+}
+
+func insertUser(db *sql.DB, task types.Task) int {
+	query := `
+	INSERT INTO tasks (monthly, weekly, deadline, description, is_completed, user_id)
+	VALUES ($1, $2, $3, $4, $5, 1)
+	RETURNING id
+	`
+
+	lastInsertId := 0
+	err := db.QueryRow(query, task.Monthly, task.Weekly, task.Deadline, task.Description, task.IsCompleted).Scan(&lastInsertId)
+	if err != nil {
+		panic(err)
+	}
+
+	return lastInsertId
+}
