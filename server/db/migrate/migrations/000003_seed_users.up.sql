5ae40cb03db795206eb558635026daf1f30e04cf
lblmdesk <loganbendev@gmail.com>
2025-04-26 14:15:10 +1200

    refactor(db): add 'jots' table
    jots are used for habit tracking

--- server/db/migrate/migrations/000003_seed_users.up.sql
+++ server/db/migrate/migrations/000003_seed_users.up.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (id, first_name, last_name, email, password, created_at)
-VALUES (1, 'john', 'doe', 'testing@gmail.com', 'test_password', CURRENT_TIMESTAMP);
+VALUES (1, 'john', 'doe', 'testing@gmail.com', '$2a$04$iUmHNc8JqzJ1K37SPlZK2eyTNoz3OJdYdAmTOHBdD1l6BZy0UBxZ2', CURRENT_TIMESTAMP);
---
+++ server/db/migrate/migrations/000005_create_jots_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS jots (
+    id SERIAL PRIMARY KEY,
+    habit varchar(15),
+    date date NOT NULL,
+    is_completed boolean DEFAULT FALSE NOT NULL,
+    user_id int REFERENCES users (id)
+);
